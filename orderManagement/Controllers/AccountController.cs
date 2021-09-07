23bbabd9095eb7c2abc6fbb77004c0bc9d7ab048
axiaobugs <49450088+axiaobugs@users.noreply.github.com>
2021-09-07 22:09:34 +1000

    feat: scope:account controller add delete and get list of roles API

--- orderManagement/Controllers/AccountController.cs
+++ orderManagement/Controllers/AccountController.cs
@@ -8,6 +8,8 @@ using orderManagement.Core.Interface;
 using orderManagement.Core.Specifications;
 using orderManagement.Dtos.Identity;
 using orderManagement.Extensions;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,32 +37,7 @@ namespace orderManagement.Controllers
             _roleManager = roleManager;
         }
         #endregion
-        /// <summary>
-        /// create a Role
-        /// </summary>
-        /// <param name="roleDto"></param>
-        /// <returns></returns>
-        [Authorize(Policy = "AdminRole")]
-        [HttpPost("role")]
-        public async Task<ActionResult<RoleDto>> CreateRole(RoleDto roleDto)
-        {
-            //Detect the role name exist
-            var roleGet = await _roleManager.FindByNameAsync(roleDto.Name);
-            if (roleGet != null) return BadRequest("Role is exist");
-            // create a new role
-            var role = new AppRole()
-            {
-                Name = roleDto.Name,
-                NormalizedName = roleDto.Name.ToUpper()
-            };
-            var result = await _roleManager.CreateAsync(role);
-            if (result.Succeeded) 
-            {
-                return Ok(_mapper.Map<RoleDto>(role));
-            }
-            // result.error then return badrequest
-            return BadRequest("Some thing wrong when create a role");
-        }
+        
 
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
@@ -122,7 +99,7 @@ namespace orderManagement.Controllers
             return Ok(await _userManager.GetRolesAsync(user));
         }
 
-        
+
         // TODO: Add Pagination with return
         [HttpGet("usersroles")]
         public async Task<ActionResult> GetUserWithRole()
@@ -160,7 +137,7 @@ namespace orderManagement.Controllers
         }
 
         /// <summary>
-        /// check username or email exist when register
+        /// Check username or email exist when register
         /// </summary>
         /// <returns></returns>
         [HttpGet("exist")]
@@ -169,16 +146,68 @@ namespace orderManagement.Controllers
         {
             if (!string.IsNullOrEmpty(email) && !string.IsNullOrWhiteSpace(email))
             {
-                return await _userManager.FindByEmailAsync(email) !=null;
+                return await _userManager.FindByEmailAsync(email) != null;
             }
             if (!string.IsNullOrEmpty(userName) && !string.IsNullOrWhiteSpace(userName))
             {
                 return await _userManager.FindByNameAsync(userName) != null;
             }
             return false;
-           
+
         }
 
+        #region CURD Role API
+        /// <summary>
+        /// Get all role
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("role")]
+        public async Task<ActionResult<ICollection<string>>> GetAllRole ()
+        {
+            return await _roleManager.Roles.Select(x=>x.Name).ToListAsync();
+        }
+
+        /// <summary>
+        /// Create a Role
+        /// </summary>
+        /// <param name="roleDto"></param>
+        /// <returns></returns>
+        [Authorize(Roles = "Admin")]
+        [HttpPost("role")]
+        public async Task<ActionResult<RoleDto>> CreateRole(RoleDto roleDto)
+        {
+            Console.WriteLine(roleDto.Name);
+            //Detect the role name exist
+            var roleGet = await _roleManager.FindByNameAsync(roleDto.Name);
+            if (roleGet != null) return BadRequest("Role is exist");
+            // create a new role
+            var role = new AppRole()
+            {
+                Name = roleDto.Name,
+                NormalizedName = roleDto.Name.ToUpper()
+            };
+            var result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return Ok(_mapper.Map<RoleDto>(role));
+            }
+            // result.error then return badrequest
+            return BadRequest("Some thing wrong when create a role");
+        }
+        /// <summary>
+        /// Delete a Role by query 
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("role")]
+        public async Task<ActionResult> DeleteRoleByName(string roleName)
+        {
+            var roleFetch = await _roleManager.FindByNameAsync(roleName);
+            if (roleFetch == null) return BadRequest("Can't found " + roleName + " role name in our database");
+            var result = await _roleManager.DeleteAsync(roleFetch);
+            return Ok(result);
+        }
+
+        #endregion
 
         #region private method
 
